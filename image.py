86c0d088cc98592584bbc9c80408c14150786ec1
Joel Crasta <loejstarc@gmail.com>
2024-09-24 15:00:18 +0530

    Image class extends Transform

--- image.py
+++ image.py
@@ -1,66 +1,36 @@
 import cv2, sys
 import logging
-from term import term_size, term_clear_full
-from genascii import generate_ascii, generate_ascii_alpha
+from transform import Transform
 
 logging.basicConfig(level=logging.INFO)
 
-class Image:
+class Image(Transform):
     def __init__(self, path):
+        super().__init__()
         self.path = path
-        self.image = None
-        self.image_gray = None
-        self.alpha_channel = None
-        self.image_gray_resized = None
-        self.alpha_channel_resized = None
-        self.ascii = None
 
     def load_image(self):
-        self.image = cv2.imread(self.path, cv2.IMREAD_UNCHANGED)
+        self.media = cv2.imread(self.path, cv2.IMREAD_UNCHANGED)
 
-        if self.image is None:
+        if self.media is None:
             raise FileNotFoundError(f"File not found: {self.path}")
-
-    def check_alpha_channel(self):
-        if self.image.shape[2] == 4:
-            self.alpha_channel = self.image[:, :, 3]
-            self.image_gray = cv2.cvtColor(self.image, cv2.COLOR_BGRA2GRAY)
-        else:
-            self.alpha_channel = None
-            self.image_gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-
-    def resize_image(self):
-        resize_width, resize_height = term_size(self.image_gray.shape)
-        self.image_gray_resized = cv2.resize(self.image_gray, (resize_width, resize_height))
-
-        if self.alpha_channel is not None:
-            self.alpha_channel_resized = cv2.resize(self.alpha_channel, (resize_width, resize_height))
-
-    def display_image(self):
-        term_clear_full()
-        if self.alpha_channel is not None:
-            ascii_image = generate_ascii_alpha(self.image_gray_resized, self.alpha_channel_resized)
-        else:
-            ascii_image = generate_ascii(self.image_gray_resized)
-
-        sys.stdout.write(ascii_image)
-        sys.stdout.flush()
-
-        self.ascii = ascii_image
-
+        
     def process_image(self) -> str:
         try:
             self.load_image()
-            self.check_alpha_channel()
-            self.resize_image()
-            self.display_image()
-
+            self.alpha()
+            self.resize()
+            self.grayscale()
+            self.display_media()
             return self.ascii
-
+        
         except FileNotFoundError as e:
             logging.error(e)
-            exit()
+            sys.exit(1)
 
         except Exception as e:
-            logging.error(f"Something went wrong:\n{e}")
-            exit()
+            logging.error(e)
+            sys.exit(1)
+
+
+
