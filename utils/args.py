dffdf2a5ba52f42a7a52c095b725bb8c94b1fbb4
Joel Crasta <loejstarc@gmail.com>
2024-10-05 19:05:05 +0530

    added invert ascii arg

--- utils/args.py
+++ utils/args.py
@@ -4,7 +4,8 @@ def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Image/Video to ASCII converter')
 
     parser.add_argument('-p','--path', type=str, default=None, help='Path to the input image or video')
-    parser.add_argument('-o', '--out', type=str, default=None, help='Path to the output text file')
-    parser.add_argument('-s', '--speed', type=int, default=5, help='Playback speed of the video (1-10, default=5)')
+    parser.add_argument('-o', '--out', type=str, default=None, help='Output file name (without extension) ')
+    parser.add_argument('-s', '--speed', type=int, default=5, choices=range(1, 11), help='Playback speed of the video (1-10, default=5 (smaller terminal font may cause video lag)')
+    parser.add_argument('-i', '--invert', action='store_true', help='Invert the ASCII character set')
 
     return parser.parse_args()
